f03685121578d7009740b7cbf2a402289907c964
Kota Nonaka <kota.kaicho@gmail.com>
2016-11-03 21:30:49 +0900

    5回演習5問目の置き場所間違えてた。直した。

---
+++ src/enshu05/Kadai05_5.java
@@ -0,0 +1,45 @@
+package enshu05;
+
+
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+public class Kadai05_5 {
+    public static void main(String[] args) {
+        try {
+            String drive = "jdbc:derby://localhost:1527/shop";
+            String sql = "select item_id,itemname,stock,expiration from t_item where expiration<current_date";
+
+            Connection con = DriverManager.getConnection(drive, "db", "db");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            
+            System.out.println("現在の日付：" + new Date(new java.util.Date().getTime()));
+            while(rs.next()) {
+                System.out.print(rs.getInt("item_id") + ":");
+                System.out.print(rs.getString("itemname") + ":");
+                System.out.print(rs.getInt("stock") + ":");
+                System.out.println(rs.getDate("expiration"));
+            }
+            
+
+            con.close();
+            stmt.close();
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
